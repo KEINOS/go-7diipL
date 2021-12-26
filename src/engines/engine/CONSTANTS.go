a8c9df4d7f86ca18ab756b48ecfd66a530d3ada4
KEINOS <github@keinos.com>
2021-12-26 14:46:14 +0900

    fix: golangci-lint gosec warning of hardcoded credentials

--- src/engines/engine/CONSTANTS.go
+++ src/engines/engine/CONSTANTS.go
@@ -1,4 +1,5 @@
 package engine
 
 // NameVarEnvAPIKeyDefault は環境変数の変数名で、翻訳 API のアクセストークンを設定するための変数名です.
+//nolint:gosec // It is not a hardcoded credentials
 const NameVarEnvAPIKeyDefault = "QIITRANS_API_KEY"
